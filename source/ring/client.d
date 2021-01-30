015fd5f0e65c18050b0a41c09a25c481ec2e306e
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-31 00:19:47 +0200

    Initialize RingClient

--- source/ring/client.d
+++ source/ring/client.d
@@ -6,12 +6,24 @@ import ring.identity;
 public final class RingClient
 {
     /**
+    * Client details
+    */
+    private RingIdentity identity;
+
+    /**
+    * Peering info
+    */
+    private RingAddress[] availablePeers;
+    private RingAddress selectedPeer;
+
+    /**
     * Constructs a new RingClient with the
     * given RingIdentity and a list of peers
     * to use for connecting
     */
     this(RingIdentity identity, RingAddress[] peers)
     {
-        
+        this.identity = identity;
+        availablePeers = peers;
     }
 }
