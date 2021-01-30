de6399527d3d381bce70a2882b672e4c32a85b77
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-31 00:10:35 +0200

    Added RingAddress class

---
+++ source/ring/address.d
@@ -0,0 +1,18 @@
+module ring.address;
+
+import std.socket : Address;
+
+public final class RingAddress
+{
+    /**
+    * Peer details
+    */
+    private string key;
+    private Address address;
+
+    this(string key, Address address)
+    {
+        this.key = key;
+        this.address = address;
+    }
+}
