fbce169ca94ce12534f827d08370cfb6a85ba225
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-31 20:25:19 +0200

    Parse config file

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ import gogga;
 import ring.client;
 import std.socket;
 import ring.address;
+import std.json;
 
 void main(string[] args)
 {
@@ -20,22 +21,25 @@ void main(string[] args)
 
 private void configStart(string filename)
 {
+	/* Open the config file, read and parse it */
 	File configFile;
 	configFile.open(filename, "rb");
 	byte[] data;
 	data.length = configFile.size();
 	configFile.rawRead(data);
 	configFile.close();
+	JSONValue config = parseJSON(cast(string)data);
 	
+	/* Read the configuration file */
 	RingAddress[] peers;
 	
 	
 	
 	
-	
+	/* Create the client and start it */
 	RingClient ringClient = new RingClient(null, getAddress("0.0.0.0", 7777)[0], peers);
 
-
+	
 
 
 }
