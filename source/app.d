9d59c03dfbc7768c9a292b4a211d4b9dda436fd4
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-31 20:18:40 +0200

    Read config file

--- source/app.d
+++ source/app.d
@@ -10,9 +10,32 @@ void main(string[] args)
 
 	if(args.length >= 2)
 	{
-
+		configStart(args[1]);
 	}
+	else
+	{
+		configStart("ring.json");
+	}
+}
 
+private void configStart(string filename)
+{
+	File configFile;
+	configFile.open(filename, "rb");
+	byte[] data;
+	data.length = configFile.size();
+	configFile.rawRead(data);
+	configFile.close();
+	
 	RingAddress[] peers;
+	
+	
+	
+	
+	
 	RingClient ringClient = new RingClient(null, getAddress("0.0.0.0", 7777)[0], peers);
+
+
+
+
 }
